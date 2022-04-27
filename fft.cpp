6a25e9c9901f22ddc3e64c674efca8694014d5bb
ebudae <48657919+ebudae@users.noreply.github.com>
2022-04-27 18:53:47 +0200

    Update fft.cpp

--- fft.cpp
+++ fft.cpp
@@ -13,26 +13,23 @@ using namespace std;
 #define pi 3.14159265358979323846
 const complex<double> i(0,1);
 
-void printlist(complex<double>* l, int N, bool extras);
-void split(complex<double> *inputs, double N);
-void fast_fourier(complex<double>* x, double N);
-double magnitude(complex<double> p, int N);
+void 	printlist(complex<double>* l, int N, bool extras);
+void 	split(complex<double> *inputs, double N);
+void 	fast_fourier(complex<double>* x, double N);
+double 	magnitude(complex<double> p, int N);
 
 /* ------ RECURSIVE IMPLEMENTATION ------- */
-
-/* Moves all even indices to 1st half
-   odd indices to 2nd half of inputs. 
-   inputs : an array of complex numbers
-   N : length of inputs array  */
-void split(complex<double> *inputs, int N)
-{
+// Moves all even indices to 1st half odd indices to 2nd half of inputs. 
+// inputs : an array of complex numbers N : length of inputs array
+void split(complex<double> *inputs, int N){
+	
 	complex<double>* even = new complex<double>[N/2];
 	complex<double>* odd = new complex<double>[N/2];
 	int ei = 0;
 	int oi = 0;
 
-	for (int j=0; j < N; j++)
-	{
+	for (int j=0; j < N; j++){
+		
 		if ((j % 2) == 0) 
 			even[ei++] = inputs[j]; 
 		else 
@@ -47,57 +44,48 @@ void split(complex<double> *inputs, int N)
 	delete[] odd;
 }
 
-/* This function computes the fast fourier 
-   transform of a list of complex numbers 
-   of length N.
-   x : input array of complex number that represent
-       a sampled function amplitude 
-   N : the length of x must be a power of 2 
-*/
+// This function computes the fast fourier transform of a list of complex numbers 
+// of length N.
+// x : input array of complex number that represent a sampled function amplitude 
+// N : the length of x must be a power of 2 
 
 void fast_fourier(complex<double>* x, double N)
 {
-	/* base of recursion */
+	// base of recursion
 	if (N == 1)return;
 	
-	/* no rounding needed if N is base 2 */
+	// no rounding needed if N is base 2
 	int n = N/2;
 
 	/* set primitive root of unity */
 	complex<double> wn = exp((2*pi*i)/N);
 	complex<double> w = 1;
 
-	/* move odd and evened indexed to each half
-	   of array x */
+	// move odd and evened indexed to each half of array x
 	split(x, 2*n);
 	
-	/* even and odd */	
+	// even and odd
 	fast_fourier(x, n);
-	/* pass pointer starting
-	at the n/2th element */
+	// pass pointer starting at the n/2th element
 	fast_fourier(x+n, n);
 
 	complex<double> even(0,0);
 	complex<double> odd(0,0);
 
-	for (int k = 0; k < n; k++)
-	{
-		/* code */
-		even = x[k];
+	for (int k = 0; k < n; k++){
+ 		even = x[k];
 		odd = x[k+n]; /* k + N/2 */
 
 		x[k] = even + w*odd;
 		x[k+n] = even - w*odd;
 
-		/* multiply same as k+1 
-		   in exponent */
 		w = w*wn;
 	}	
 }
 
 void ifft(complex<double>* x, double N)
 {
-	/* base of recursion */
+	// base of recursion
 	if (N == 1)return;
 	
 	/* no rounding needed if N is base 2 */
@@ -107,66 +95,53 @@ void ifft(complex<double>* x, double N)
 	complex<double> wn = exp((-2*pi*i)/N)/N;
 	complex<double> w = 1;
 
-	/* move odd and evened indexed to each half
-	   of array x */
+	// move odd and evened indexed to each half of array x
 	split(x, 2*n);
 	
-	/* even and odd */	
+	// even and odd	
 	ifft(x, n);
-	/* pass pointer starting
-	at the n/2th element */
+	// pass pointer starting at the n/2th element
 	ifft(x+n, n);
 
 	complex<double> even(0,0);
 	complex<double> odd(0,0);
 
-	for (int k = 0; k < n; k++)
-	{
-		/* code */
+	for (int k = 0; k < n; k++){
 		even = x[k];
 		odd = x[k+n]; /* k + N/2 */
 
 		x[k] = even + w*odd;
 		x[k+n] = even - w*odd;
 
-		/* multiply same as k+1 
-		   in exponent */
 		w = w*wn;
 	}	
 }
 
-
-/* calculated the magnitude of a complex number 
-   returned by the fft */
+// calculate the magnitude of a complex number 
 double magnitude(complex<double> p, int N) 
 { 
 	return 2*sqrt(pow(p.real(),2) + pow(p.imag(),2))/N;
 }
 
-/* truncate very small numbers to 0 */
-double approx_zero(double d) 
-{ 
-	if (abs(d) < 0.0000000000001)
-		return 0;
-	else 
-		return d;
+// truncate very small numbers to 0
+double approx_zero(double d) { 
+	if (abs(d) < 0.0000000000001)	return 0;
+	else 				return d;
 }
 
 /* Default behavior prints the contents of a list.
    When extras == TRUE, computes the magnitude
    and phase of complex inputs and displays */
-void printlist(complex<double>* l, int N, bool extras)
-{
+void printlist(complex<double>* l, int N, bool extras){
+	
 	for (int i = 0; i < N; i++)
 		cout << " " << l[i];
 
 	double mag = 0;
 	double phase = 0; 
 
-	if (extras)
-	{
-		for (int i = 0; i < N; i++)
-		{
+	if (extras){
+		for (int i = 0; i < N; i++){
 			mag = magnitude(l[i],N);
 			phase = atan(l[i].imag()/l[i].real());
 
@@ -180,9 +155,8 @@ void printlist(complex<double>* l, int N, bool extras)
 }
 
 
-/* ------ Main --------- */
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char* argv[]){
+	
 	double amplitude = 0;
 	/* change samples for more data points */
 	int samples = 16;
@@ -221,7 +195,6 @@ int main(int argc, const char* argv[])
 
 	cout << "Output array: " << endl;
 	printlist(s,samples,TRUE);
-
 
 	/* send values to file for plotting */
 	ofstream outdata;
